64a6a7491abae4a0df1ccd28e337e7a906d39dd7
maryamjahanbakhshi <maryamjahanbakhshi008@gmail.com>
2025-12-13 19:23:01 +0330

    check youser

--- src/app/public/pages/loginpage/loginpage.ts
+++ src/app/public/pages/loginpage/loginpage.ts
@@ -1,21 +1,28 @@
+import { IfStmt } from '@angular/compiler';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loginpage.html',
   styleUrl: './loginpage.scss',
 })
 export class Loginpage {
-  router=inject(Router)
+  message: string = '';
+  router = inject(Router)
   loginForm: LoginForm = {
     userName: '',
     password: '',
     keepMe: false
   };
-  test(){
-    
+  test() {
+    if (this.loginForm.userName == 'maryam' && this.loginForm.password == '1382') {
+      this.router.navigateByUrl('private')
+    } else {
+      this.message = 'نام کاربری یا کلمه عبور اشتباه است ';
+    }
   }
 }
 
