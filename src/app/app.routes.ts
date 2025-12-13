2f58cdfcd98f430bb3c6f20ad9c0fca18d180649
maryamjahanbakhshi <maryamjahanbakhshi008@gmail.com>
2025-12-13 12:45:35 +0330

    basic setting , add font folder and craet pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Loginpage } from './public/pages/loginpage/loginpage';
+import { PrivateTemplat } from './private/private-templat/private-templat';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Loginpage },
+    {
+        path: 'private', component: PrivateTemplat, children: [
+
+        ]
+    },
+    {path:'', redirectTo:'login',pathMatch:'full'},
+    {path:'**',redirectTo:'login'}
+];
---
+++ src/app/private/private-templat/private-templat.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-private-templat',
+  imports: [RouterOutlet],
+  templateUrl: './private-templat.html',
+  styleUrl: './private-templat.scss',
+})
+export class PrivateTemplat {
+
+}
